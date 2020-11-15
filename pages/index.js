6d0044a9279d69e131efa8a4543ece4efd032a05
gbsinvents <guilhermebiazisabin@gmail.com>
2020-11-15 14:48:04 -0300

    teste com kepler.gl

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <div>Hello World</div>
+    return (
+        <div>
+            <iframe src="kepler.gl.html"></iframe>
+        </div>
+    )
 }
 
 export default Home
